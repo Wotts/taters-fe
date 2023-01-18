9efe86d0a002f671c305636c05c57af4bfec84c6
Wotts <twan.3ssen@gmail.com>
2023-01-18 17:35:47 +0100

    Add semicolons to main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import './style.css'
-import App from './App.vue'
-import router from './router'
+import { createApp } from 'vue';
+import { createPinia } from 'pinia';
+import './style.css';
+import App from './App.vue';
+import router from './router';
 
-const pinia = createPinia()
+const pinia = createPinia();
 
-const app = createApp(App)
+const app = createApp(App);
 
-app.use(pinia).use(router).mount('#app')
+app.use(pinia).use(router).mount('#app');
