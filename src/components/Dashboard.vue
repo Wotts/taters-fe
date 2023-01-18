aec9a5c63a80a366faa081074c2c673eb8c9604a
Wotts <twan.3ssen@gmail.com>
2023-01-18 19:57:30 +0100

    Add Home and add logout to Dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,6 +1,14 @@
 <script setup lang="ts">
+import { useAuthStore } from '@/stores/authStore';
+
+const auth = useAuthStore();
 </script>
 
 <template>
-  <p>this is a dashboard</p>
+  <span>
+    Welcome to your dashboard. You've peeled 63719812 potatoes this week.
+    That's not enough, Robert.
+    You still have 36280188 potatoes to go.
+  </span>
+  <button @click="auth.logout()">Logout</button>
 </template>
---
+++ src/components/Home.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { useAuthStore } from '@/stores/authStore';
+import router from '@/router';
+
+const auth = useAuthStore();
+</script>
+
+<template>
+  <div class="column">
+    <p>Welcome back, Robert.</p>
+    <button @click="router.push({name: 'Login'})">Login</button>
+    <button @click="router.push({name: 'Dashboard'})">Dashboard</button>
+    <button v-if="auth.isAuthenticated()" @click="auth.logout()">Logout</button>
+  </div>
+</template>
