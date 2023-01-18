0be2ed760fec231c5538daa3c0c38566f29106fa
Wotts <twan.3ssen@gmail.com>
2023-01-18 20:40:13 +0100

    Go Home after logging out

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,6 +1,7 @@
 import { defineStore } from "pinia";
 import { useStorage } from '@vueuse/core';
 import { Authorization } from '@/enums/auth';
+import router from "@/router";
 
 export const useAuthStore = defineStore('auth', () => {
   const authState = useStorage('authenticated', false);
@@ -22,6 +23,7 @@ export const useAuthStore = defineStore('auth', () => {
 
   const logout = (): void => {
     authState.value = false;
+    router.push({ name: 'Home'});
   }
 
   return { isAuthenticated, login, logout };
